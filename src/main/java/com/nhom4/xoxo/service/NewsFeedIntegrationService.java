a19bf7564981a00d36041e1fa64063af86e1b475
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-09-27 17:49:52 +0700

    fix

--- src/main/java/com/nhom4/xoxo/service/NewsFeedIntegrationService.java
+++ src/main/java/com/nhom4/xoxo/service/NewsFeedIntegrationService.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Service
 @Slf4j
-@Transactional
+@Transactional("transactionManager")
 public class NewsFeedIntegrationService {
     
     private final NewsFeedService newsFeedService;
